7529d107f12b41da2741615513043b300d56412c
Yizheng Shen <tinirdoc@gmail.com>
2015-06-19 17:40:58 -0500

    sample app intro page updated

--- ChatAppInfoPage/public/javascripts/index.js
+++ ChatAppInfoPage/public/javascripts/index.js
@@ -1,7 +1,6 @@
 if ($(window).height() < $(window).width()) {
   $("#section_design__feature p").width($("#section_design__feature").width() - $("#section_design__feature img").width()*2 - 40);
-}
-
-$(window).resize(function() {
   window.location.reload();
-});
+} else {
+  window.location.reload();
+}
