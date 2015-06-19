0a6c2d23814edde327f02a50db613b9d695f162c
Yizheng Shen <tinirdoc@gmail.com>
2015-06-19 17:38:01 -0500

    sample app intro page updated

--- ChatAppInfoPage/public/javascripts/index.js
+++ ChatAppInfoPage/public/javascripts/index.js
@@ -1,3 +1,7 @@
 if ($(window).height() < $(window).width()) {
   $("#section_design__feature p").width($("#section_design__feature").width() - $("#section_design__feature img").width()*2 - 40);
 }
+
+$(window).resize(function() {
+  window.location.reload();
+});
