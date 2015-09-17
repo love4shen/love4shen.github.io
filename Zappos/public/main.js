e1920e3753325b3dd8acc5adf1b2731f813c21c3
Yizheng Shen <tinirdoc@gmail.com>
2015-09-16 22:42:56 -0500

    scroll speed bug fixed, need further tweek

--- Zappos/public/main.js
+++ Zappos/public/main.js
@@ -1,16 +1,21 @@
 var lastPos = null;
 var timer = 0;
-
+var display = false;
 
 window.onscroll = function(){
   var body = document.getElementsByTagName('body')[0];
-  body.className = body.className.replace(/\bdisplayMenu\b/,'');
 
   var currentPos = window.scrollY;
   if ( lastPos != null ){
     var delta = currentPos - lastPos;
-    if (delta < 0) {
+    console.log(delta);
+    if (delta < -50 && !display) {
+      body.className = body.className.replace(/\bdisplayMenu\b/,'');
       body.className += 'displayMenu';
+      display = true;
+    } else if (delta > 10 && display) {
+      body.className = body.className.replace(/\bdisplayMenu\b/,'');
+      display = false;
     }
   }
   lastPos = currentPos;
