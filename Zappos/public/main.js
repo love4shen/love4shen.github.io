4ed07368d56fca992003d246d903a6a18aa8651f
Yizheng Shen <tinirdoc@gmail.com>
2015-09-19 23:38:28 -0500

    tune js file

--- Zappos/public/main.js
+++ Zappos/public/main.js
@@ -5,19 +5,20 @@ var body = document.getElementsByTagName('body')[0];
 
 window.onscroll = function () {
   var currentPos = window.scrollY;
-  if (lastPos != null) {
+  if (lastPos !== null) {
     var delta = currentPos - lastPos;
+    body.className = body.className.replace(/\bdisplayMenu\b/,'');
     if (delta < -50 && !display) {
-      body.className = body.className.replace(/\bdisplayMenu\b/,'');
       body.className += 'displayMenu';
       display = true;
     } else if (delta > 10 && display) {
-      body.className = body.className.replace(/\bdisplayMenu\b/,'');
       display = false;
     }
   }
   lastPos = currentPos;
-  timer && clearTimeout(timer);
+  if (timer) {
+    clearTimeout(timer);
+  }
   timer = setTimeout(function () {
     lastPos = null;
   }, 100);
@@ -28,11 +29,11 @@ var navLinks = document.querySelectorAll('nav > ul > a');
 for (var i in navLinks) {
   navLinks[i].onclick = function () {
     body.className = body.className.replace(/\bdisplayMenu\b/,'');
-  }
+  };
 }
 
 console.log('%c Happy to see you here!', 'background-color: #ffcc33; color: #7a0019; font-weight: bold; font-size: 2rem;');
 console.log('');
-console.log('Scroll up fast enough to unveil the navigation menu and scroll down to hide it again');
+console.log('Scroll up fast enough to unveil the navigation menu and scroll down to hide it');
 console.log('');
 console.log('[.__.]');
